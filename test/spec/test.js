1c41913bc60ee7d5761af3ba67d601b747f6553a
Ain Tohvri <at@interactive-pioneers.de>
2015-05-08 18:07:01 +0200

    Fix JSCS errors in spec

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,18 +1,18 @@
 'use strict';
 /* jshint undef: false */
-(function () {
-  describe('MlhMenu', function () {
+(function() {
+  describe('MlhMenu', function() {
 
     var menu = null;
 
-    describe('init', function () {
+    describe('init', function() {
       beforeEach(function() {
         menu = $('#mocha').mlhmenu();
       });
-      it('expected to construct object', function () {
+      it('expected to construct object', function() {
         return expect(menu).to.exist;
       });
-      it('expected to return element initialised on', function () {
+      it('expected to return element initialised on', function() {
         return expect($(menu).attr('id')).to.equal('mocha');
       });
     });
